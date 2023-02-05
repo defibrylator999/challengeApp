87c496ca84ed38bdd32e85064488ba7bdac22b68
Sławek <defibrylator999@o2.pl>
2023-02-05 21:27:25 +0100

    listy i tablice

--- Program.cs
+++ Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//cwiczenie tablice
+string[] mounth = new string[12];
+mounth[0] = "-styczeń";
+mounth[1] = "-luty";
+mounth[2] = "-marzec";
+mounth[3] = "-kwiecień";
+mounth[4] = "-maj";
+mounth[5] = "-czerwiec";
+mounth[6] = "-lipiec";
+mounth[7] = "-sierpień";
+mounth[8] = "-wrzesień";
+mounth[9] = "-październik";
+mounth[10] = "-listopad";
+mounth[11] = "-grudzień";
+
+for (var i = 0; i < mounth.Length; i=i+2)
+{
+    Console.WriteLine(mounth[i]);
+}
+
+//cwiczenie listy,
+List<string> mounth1 = new List<string>();
+mounth1.Add ("1 styczeń");
+mounth1.Add ("1 luty");
+mounth1.Add ("1 marzec");
+mounth1.Add ("1 kwiecień");
+mounth1.Add ("1 maj");
+mounth1.Add ("1 czerwiec");
+mounth1.Add ("1 lipiec");
+mounth1.Add ("1 sierpień");
+mounth1.Add ("1 październik");
+mounth1.Add ("1 listopad");
+mounth1.Add ("1 grudzień");
+
+for (var i=0; i < mounth1.Count; i = i + 2)
+{
+    Console.WriteLine(mounth1[i]);
+}
