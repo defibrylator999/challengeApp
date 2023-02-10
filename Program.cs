545246ae5c207cf3b1d2cd56f104938af49bb158
Sławek <defibrylator999@o2.pl>
2023-02-10 21:08:36 +0100

    notatki tablice oraz listy petla for i foreatch

--- Program.cs
+++ Program.cs
@@ -1,38 +1,87 @@
-﻿//cwiczenie tablice
-string[] mounth = new string[12];
-mounth[0] = "-styczeń";
-mounth[1] = "-luty";
-mounth[2] = "-marzec";
-mounth[3] = "-kwiecień";
-mounth[4] = "-maj";
-mounth[5] = "-czerwiec";
-mounth[6] = "-lipiec";
-mounth[7] = "-sierpień";
-mounth[8] = "-wrzesień";
-mounth[9] = "-październik";
-mounth[10] = "-listopad";
-mounth[11] = "-grudzień";
+﻿//listy:
+List<string> dayOfWeeks4 = new List<string>();
+dayOfWeeks4.Add("poniedziałek");
+dayOfWeeks4.Add("wtorek");
+dayOfWeeks4.Add("środa");
+dayOfWeeks4.Add("czwarterk");
+dayOfWeeks4.Add("piątek");
+dayOfWeeks4.Add("sobota");
+dayOfWeeks4.Add("niedziela");
+for (var i = 0; i < dayOfWeeks4.Count; i++)
+{
+    Console.WriteLine(dayOfWeeks4[i]);
+}
 
-for (var i = 0; i < mounth.Length; i=i+2)
+
+//tablice
+string[] mounth = new string[12];
+mounth[0] = "styczeń";
+mounth[1] = "luty";
+mounth[2] = "marzec";
+mounth[3] = "kwiecien";
+mounth[4] = "maj";
+mounth[5] = "czerwiec";
+mounth[6] = "lipiec";
+mounth[7] = "sierpien";
+mounth[8] = "wrzesien";
+mounth[9] = "pażdziernik";
+mounth[10] = "listopad";
+mounth[11] = "grudzień";
+
+for (var i = 0; i < mounth.Length; i++)
 {
     Console.WriteLine(mounth[i]);
 }
 
-//cwiczenie listy,
-List<string> mounth1 = new List<string>();
-mounth1.Add ("1 styczeń");
-mounth1.Add ("1 luty");
-mounth1.Add ("1 marzec");
-mounth1.Add ("1 kwiecień");
-mounth1.Add ("1 maj");
-mounth1.Add ("1 czerwiec");
-mounth1.Add ("1 lipiec");
-mounth1.Add ("1 sierpień");
-mounth1.Add ("1 październik");
-mounth1.Add ("1 listopad");
-mounth1.Add ("1 grudzień");
 
-for (var i=0; i < mounth1.Count; i = i + 2)
+//listy:
+List<string> coulors = new List<string>();
+coulors.Add("black");
+coulors.Add("blue");
+coulors.Add("green");
+coulors.Add("yelow");
+coulors.Add("red");
+coulors.Add("vaylet");
+coulors.Add("braun");
+coulors.Add("orange");
+coulors.Add("gold");
+coulors.Add("silver");
+coulors.Add("pink");
+coulors.Add("white");
+
+Console.WriteLine(coulors[0]);
+
+for (var i = 0; i < coulors.Count; i = i + 1)
 {
-    Console.WriteLine(mounth1[i]);
+    Console.WriteLine(coulors[i]);
 }
+
+
+
+foreach (var nic in coulors)
+{
+    Console.WriteLine(nic);
+}
+
+string[] mounthh = new string[12];
+mounthh[0] = "styczeń";
+mounthh[1] = "luty";
+mounthh[2] = "marzec";
+mounthh[3] = "kwiecien";
+mounthh[4] = "maj";
+mounthh[5] = "czerwiec";
+mounthh[6] = "lipiec";
+mounthh[7] = "sierpien";
+mounthh[8] = "wrzesien";
+mounthh[9] = "pażdziernik";
+mounthh[10] = "listopad";
+mounthh[11] = "grudzień";
+
+foreach (var mount in mounthh)
+{
+    Console.WriteLine(mount);
+}
+
+
+
+return 0;
